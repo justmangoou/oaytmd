5d081fb59ed5ec82464ebc1a91599334098b148d
Tuan Nguyen <38831897+justmangoou@users.noreply.github.com>
2026-06-04 00:39:51 +0700

    fix: minor things

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 <output_directory> <binary_name> <target_triple>"
-	echo "Example: $0 ~/.config/opendeck/plugins/justmangoou.oaytmd.sdPlugin oaytmd x86_64-unknown-linux-gnu"
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <output_directory> <target_triple>"
+	echo "Example: $0 ~/.config/opendeck/plugins/justmangoou.oaytmd.sdPlugin x86_64-unknown-linux-gnu"
 	exit 1
 fi
 
@@ -16,4 +16,4 @@ rm -rf "$1"
 cp -r assets/ "$1"
 
 cargo build --release
-cp target/release/$2 "$1/$2-$3"
+cp target/release/oaytmd "$1/oaytmd-$2"
