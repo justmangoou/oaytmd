bcf0512088d1c53fbe6520aa9a11dfdc511906dc
Tuan Nguyen <38831897+justmangoou@users.noreply.github.com>
2026-06-04 00:51:11 +0700

    fix: ensure state is updated

--- src/ws_events.rs
+++ src/ws_events.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
 
 use oaytmd_companion::models::response::{StateResponse, WebsocketEvent};
+use openaction::{Action, visible_instances};
 
-use crate::client::{PlayerWrapper, ytmd_player};
+use crate::{actions::RepeatAction, client::{PlayerWrapper, ytmd_player}};
 
 pub async fn handle_ws_event(item: WebsocketEvent) {
 	match item {
@@ -23,4 +24,14 @@ async fn apply_state_update(state: StateResponse) {
 			.map(|q| q.repeat_mode)
 			.unwrap_or_default(),
 	}));
+
+	call_did_receive_settings(RepeatAction::UUID).await;
+}
+
+async fn call_did_receive_settings(action_uuid: &'static str) {
+    for instance in visible_instances(action_uuid).await {
+        if let Err(e) = instance.get_settings().await {
+            log::error!("Failed to call did_receive_settings for action {}: {}", action_uuid, e);
+        }
+    }
 }
